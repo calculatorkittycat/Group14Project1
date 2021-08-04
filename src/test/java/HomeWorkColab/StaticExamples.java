3da2fd315ba95d4f068bcbe38ecdc75fe39906f3
calculatorkittycat <cicain90@gmail.com>
2021-08-03 20:21:49 -0700

    what i just did

---
+++ src/test/java/HomeWorkColab/StaticExamples.java
@@ -0,0 +1,36 @@
+package HomeWorkColab;
+
+public class StaticExamples {
+
+    public String name, gender;
+    public int age, ID;
+    public static String schoolName = "Cybertek School";
+    public static String programmingLanguage = "Java";
+
+
+    public void printName(){
+        System.out.println(name);
+    }
+
+    public static void printSchoolName(){
+        System.out.println(schoolName);
+    }
+
+    public void method2(){
+        System.out.println(schoolName);
+    }
+
+    public void getInfo(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(ID);
+        System.out.println(schoolName);
+        System.out.println(programmingLanguage);
+    }
+
+
+
+
+
+}
---
+++ src/test/java/HomeWorkColab/scrumHomework/Tester.java
@@ -0,0 +1,53 @@
+package HomeWorkColab.scrumHomework;
+
+public class Tester {
+
+    public String name;
+    public int employeeID;
+    public String jobTitle;
+    public double salary;
+
+
+
+}
+
+/*
+ create a class called Tester
+        Attributes:
+            name, employeeID, JobTitle, Salary
+        Actions:
+           setInfo(), smokeTesting(),  creatingTicket(), dailyStandUp() toString()
+
+create a class called Developer
+          Attributes:
+            name, employeeID, JobTitle, Salary
+          Actions:
+            setInfo(), coding(), unitTesting(), fixingBug(), toString()
+
+create a class called ScrumTeam
+           Attributes:
+                String PO, BA, SM;
+                ArrayList<Tester> testersList = new ArrayList<>();
+                ArrayList<Developer> devopsList = new ArrayList<>();
+               int daysOfSprint;
+
+             Actions:
+                  setInfo(): sets the names of PO, BA, SM
+                  addTester(Tester tester): adds the given tester to the testers ArrayList
+
+                  addTesters(Tester[] testers): adds the given testers to the testers ArrayList
+
+                  addDeveloper(Developer developer): adds the given developer to the developers ArrayList
+
+                  addDevelopers(Developer[] developers): adds the given developers to the developers ArrayList
+                 removeTester(long employeeID): removes the given tester from the testers ArrayList
+
+                 removeDeveloper(long employeeID): removes the developer from the developers ArrayList
+
+                  toString(): prints number of tester,& developers,  PO name, SM name, BA name
+
+create a class called MyScrumTeam:
+                    1. create an array of Testers and add the testers from your group
+                    2. create an array of developers add the developers from your group
+                    3. create an object of ScrumTeam and store the testers & developers above to the scrum team
+ */
