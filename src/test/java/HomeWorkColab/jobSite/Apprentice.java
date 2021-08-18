e060a6ee6cfff6a754a21cc929a479c57cdc2a09
calculatorkittycat <cicain90@gmail.com>
2021-08-17 20:09:58 -0700

    started homework 8/17/2021

---
+++ src/test/java/HomeWorkColab/jobSite/Apprentice.java
@@ -0,0 +1,4 @@
+package HomeWorkColab.jobSite;
+
+public class Apprentice {
+}
---
+++ src/test/java/HomeWorkColab/jobSite/Worker.java
@@ -0,0 +1,48 @@
+package HomeWorkColab.jobSite;
+
+public class Worker {
+
+    private final String name;
+    public final static boolean hasTools, hasVan;
+    public final static double gasPrice;
+    public boolean isContractor;                     //to make private or public all?
+    public boolean isEmployee;
+
+    static {
+        hasTools = true;
+        hasVan = true;
+        gasPrice = 3.89;
+    }
+
+    public Worker(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double payRate(){
+        return 0;
+    }
+
+    public int hoursPerWeek(){
+        return 0;
+    }
+
+    public double milesToWork(){
+        return 0;
+    }
+
+
+
+
+}
+/*
+payRate
+milesToWork
+hoursPerWeek
+gasPrice
+isContractor // If true, must pay 10000$ per year tax
+isEmployee // if true must pay 11% tax per pay check
+ */
