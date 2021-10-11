0c00af638d7983f177942cd8b72a45b2b4ceebf8
calculatorkittycat <cicain90@gmail.com>
2021-10-10 17:28:04 -0700

    started homework 8/17/2021

--- src/test/java/Abstraction/CampusStudent.java
+++ src/test/java/Abstraction/CampusStudent.java
@@ -5,5 +5,9 @@ public class CampusStudent extends Student {
     @Override
     public void attendClass() {
         System.out.println("Campus student is attending class in person");
+
+
+        int a = 10;
+
     }
 }
